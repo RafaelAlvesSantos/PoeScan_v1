9107775f645771feb65ab316df8581dcfb675adb
RafaelAlvesSantos <rafaelalvesmgs@gmail.com>
2020-03-19 19:54:48 -0300

    images for thumbnails added, no regular classes yet

--- src/components/characters/Characters.js
+++ src/components/characters/Characters.js
@@ -11,7 +11,7 @@ const Characters = () => {
 
   const getCharacterLadder = async () => {
     const res = await axios.get(
-      "http://api.pathofexile.com/ladders/Metamorph?limit=3"
+      "http://api.pathofexile.com/ladders/Metamorph?limit=10"
     );
     setChars(res.data.entries);
   };
